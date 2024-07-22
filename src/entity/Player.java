273578a01891364ccc81f8e5e1ee0424f79e888d
rafaelmslima <rafael.mslima@upe.br>
2024-07-22 20:05:05 -0300

    Melhorei a SolidArea do player para que ele não fique travado em locais com passagem pequena

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -25,8 +25,8 @@ public class Player extends Entity {
         screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
 
         solidArea = new Rectangle();
-        solidArea.x = 8;
-        solidArea.y = 16;
+        solidArea.x = 5;
+        solidArea.y = 8;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y; // guardando os valores default pois irei trocar os valores x e y depois
         solidArea.width = 32;
